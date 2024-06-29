5919c999b0a43288e4ead9cb1d4f1950cc71f1fa
archanatudu <166447117+archanatudu@users.noreply.github.com>
2024-06-29 17:25:06 +0530

    todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import TaskList from './component/TaskList'
 import CompletedTask from './component/CompletedTask'
+import './App.scss';
 
 const App:React.FC = () => {
   return (
     <>
-    <TaskList/>
-    <CompletedTask/>
+      <div className ="todo_app_section">
+    
+        <div className ="todo_app_container">
+
+          <div className="new_task_list task_list">
+            <h1>Task List</h1>
+            <div>
+              <TaskList/>
+            </div>
+          </div>
+
+        
+          <div className="completed_task_list task_list">
+            <h1> Completed Task</h1>
+            <div>
+              <CompletedTask/>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
